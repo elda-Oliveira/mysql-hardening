4d2eff7dcf3e3c5ebcc713b12672de74cd599298
Elda-Oliveira <elda.paz@aluno.faculdadeimpacta.com.br>
2025-07-04 21:28:35 -0300

    Update alerta_mysql.ps1

--- scripts/alerta_mysql.ps1
+++ scripts/alerta_mysql.ps1
@@ -1,1 +1,6 @@
 
+# alerta_mysql.ps1
+if ((Get-Service MySQL).Status -ne "Running") {
+  Write-Output "MySQL service is down!"
+  # Aqui você pode adicionar envio de alerta por Telegram, e-mail, etc.
+}
